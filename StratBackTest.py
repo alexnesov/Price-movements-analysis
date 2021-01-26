cf0a9ccde7af54a248fc45f6c051606fd53cbb5c
alexnesov <alexandrenesovic@gmail.com>
2021-01-26 21:29:11 +0100

    filter

--- StratBackTest.py
+++
@@ -1,7 +0,0 @@
-"""
-Objective is to know:
-
-In the CONTEXT of my trend reversal strategy,
-can I know when is the best time to leave a position.
-i. e.: 
-"""
---
+++ filter.py
@@ -0,0 +1,16 @@
+"""
+
+Problem: 
+Many signals are given
+BUT, only a few can be selected to have a reasonable amount
+of stocks in the portfolio
+
+Objective is to know:
+
+In the CONTEXT of my trend reversal strategy,
+1. can I know when is the best time to leave a position.
+2. How can I enhance the selection of those stocks to have a reduced batch of choices, 
+and a globally better batch
+3. The holding period should be max 15 days.
+
+"""
